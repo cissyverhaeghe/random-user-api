daa3a92f97f16386b80f7cda16f1eeea0e4c4ff4
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-02-18 15:42:22 +0100

    you can filter but everytime there are 200 new people

--- js/index.js
+++ js/index.js
@@ -7,12 +7,14 @@ getData();
 //if anything is typed in the searchfield, render
 veld.oninput = function (e) {
   if (e.target.value.length >= 3) {
+    render(e.target.value);
+  }
+  if (e.target.value.length < 3) {
     getData();
-    //console.log(e.target.value);
   }
 };
 
-
+//function to get 200 people from the database
 async function getData() {
   const response = await fetch("https://randomuser.me/api/?results=200");
   const data = await response.json();
@@ -45,6 +47,42 @@ async function getData() {
     .join("");
 }
 
+async function render(zoekStr = "") {
+  const response = await fetch("https://randomuser.me/api/?results=200");
+  const data = await response.json();
+  lijst.innerHTML = data.results
+    .filter(
+      (Person) =>
+        Person.name.first.indexOf(zoekStr) !== -1 ||
+        Person.name.last.indexOf(zoekStr) !== -1
+    )
+    .map(
+      (Person) => `<div class="column is-half-tablet is-one-quarter-desktop">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-1by1">
+              <img
+                src="${Person.picture.thumbnail}"
+                alt="${Person.name.first} ${Person.name.last}"
+              />
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">${Person.name.first} ${Person.name.last}</p>
+                <p class="subtitle is-6">${Person.email}</p>
+              </div>
+            </div>
+            <div class="content">
+              <p>Country: ${Person.location.country}</p>
+            </div>
+          </div>
+        </div>
+      </div>`
+    )
+    .join("");
+}
 // document.querySelector('#voornaam').oninput = function(e){
 //     console.log(e.target.value);
 // }
