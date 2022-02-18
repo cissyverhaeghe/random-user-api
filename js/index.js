a3f9657c4d8d47060541dd27ab07bdcd1ce0bfc5
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-02-18 14:20:33 +0100

    200 results appear when you type something in the search bar

--- js/index.js
+++ js/index.js
@@ -1,3 +1,35 @@
+const veld = document.querySelector("#zoekString");
+const lijst = document.querySelector(".columns");
+
+//if anything is typed in the searchfield, render
+veld.oninput = function (e) {
+  if (e.target.value.length >= 3) {
+    getData();
+    //console.log(e.target.value);
+  }
+};
+
+async function getData() {
+  const response = await fetch("https://randomuser.me/api/?results=200");
+  const data = await response.json();
+  lijst.innerHTML = data.results
+    .map(
+      (
+        Person
+      ) => `<div class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen is-fifth-fullhd">
+  <div class="card">
+    <div class="card-image">
+      <h2>${Person.name.first}</h2>
+      <figure class="image is-3by4">
+        <img src="${Person.picture.thumbnail}" alt="${Person.name.first}" />
+      </figure>
+    </div>
+  </div>
+</div>`
+    )
+    .join("");
+}
+
 // document.querySelector('#voornaam').oninput = function(e){
 //     console.log(e.target.value);
 // }
@@ -9,5 +41,5 @@
 // }
 
 // function render(zoekStr=""){
-// 	data.filter().map()	
+// 	data.filter().map()
 // }
