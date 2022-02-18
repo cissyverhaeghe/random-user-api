56612331aec98879d921502c0a0d83261e7807bd
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-02-18 15:13:13 +0100

    200 users are shown when opening the page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const veld = document.querySelector("#zoekString");
 const lijst = document.querySelector(".columns");
 
+//get 200 people when opening the page
+getData();
+
 //if anything is typed in the searchfield, render
 veld.oninput = function (e) {
   if (e.target.value.length >= 3) {
@@ -9,23 +12,35 @@ veld.oninput = function (e) {
   }
 };
 
+
 async function getData() {
   const response = await fetch("https://randomuser.me/api/?results=200");
   const data = await response.json();
   lijst.innerHTML = data.results
     .map(
-      (
-        Person
-      ) => `<div class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen is-fifth-fullhd">
-  <div class="card">
-    <div class="card-image">
-      <h2>${Person.name.first}</h2>
-      <figure class="image is-3by4">
-        <img src="${Person.picture.thumbnail}" alt="${Person.name.first}" />
-      </figure>
-    </div>
-  </div>
-</div>`
+      (Person) => `<div class="column is-half-tablet is-one-quarter-desktop">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-1by1">
+              <img
+                src="${Person.picture.thumbnail}"
+                alt="${Person.name.first} ${Person.name.last}"
+              />
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">${Person.name.first} ${Person.name.last}</p>
+                <p class="subtitle is-6">${Person.email}</p>
+              </div>
+            </div>
+            <div class="content">
+              <p>Country: ${Person.location.country}</p>
+            </div>
+          </div>
+        </div>
+      </div>`
     )
     .join("");
 }
