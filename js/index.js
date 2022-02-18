077d59a875d0c7a8e603f37b439b05e4f92be26e
Cissy <91531100+cissyverhaeghe@users.noreply.github.com>
2022-02-18 11:45:03 +0100

    inital commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// document.querySelector('#voornaam').oninput = function(e){
+//     console.log(e.target.value);
+// }
+
+// document.querySelector('#voornaam').oninput = function(e){
+// 	if(e.target.value.length >= 3){
+// 		render(e.target.value)
+// 	}
+// }
+
+// function render(zoekStr=""){
+// 	data.filter().map()	
+// }
